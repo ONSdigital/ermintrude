ed4e27d71c0dbe6b711a95a893f8cd162ff17104
pastor-methods <pastor.methods@gmail.com>
2015-12-17 14:38:34 +0000

    Delete collection cookie on logout

--- src/main/web/ermintrude/js/main.js
+++ src/main/web/ermintrude/js/main.js
@@ -269,6 +269,7 @@ function handleApiError(response) {
  */
 function logout() {
   delete_cookie('access_token');
+  delete_cookie('collection');
   localStorage.setItem("loggedInAs", "");
   Ermintrude.refreshAdminMenu();
   viewController();
@@ -341,45 +342,6 @@ function postPassword(success, error, email, password, oldPassword) {
         error(response);
       } else {
         handleApiError(response);
-      }
-    }
-  });
-}
-
-/**
- * Post to the zebedee API permission endpoint.
- * Set permissions for the given email address.
- * @param success
- * @param error
- * @param email - The email of the user to set permissions for
- * @param admin - boolean true if the user should be given admin permissions
- * @param editor - boolean true if the user should be given editor permissions
- */
-function postPermission(success, error, email, admin, editor) {
-  $.ajax({
-    url: "/zebedee/permission",
-    dataType: 'json',
-    contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({
-      email: email,
-      admin: admin,
-      editor: editor
-    }),
-    success: function () {
-      if(success) {
-        success();
-      }
-    },
-    error: function (response) {
-      if(error) {
-        error(response);
-      } else {
-        if (response.status === 403 || response.status === 401) {
-          sweetAlert("You are not permitted to update permissions.")
-        } else {
-          handleApiError(response);
-        }
       }
     }
   });
