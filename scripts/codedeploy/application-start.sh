53279a6afd99a6fa87dd156d997360b0b012d552
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-06-29 11:35:59 +0100

    Only bundle/deploy develop

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -3,9 +3,10 @@
 ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
-docker run -d --name ermintrude           \
-    --env=BABBAGE_URL=http://babbage:8080 \
-    --env=ZEBEDEE_URL=http://zebedee:8080 \
-    --net=publishing                      \
-    --restart=always                      \
-    $ECR_REPOSITORY_URI/ermintrude:$GIT_COMMIT
+docker run -d                           \
+  --env=BABBAGE_URL=http://babbage:8080 \
+  --env=ZEBEDEE_URL=http://zebedee:8080 \
+  --name=ermintrude                     \
+  --net=publishing                      \
+  --restart=always                      \
+  $ECR_REPOSITORY_URI/ermintrude:$GIT_COMMIT
