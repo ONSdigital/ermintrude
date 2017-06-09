c6e477bee185da882d2fc428cfc8ec6b5ed12283
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2017-06-09 14:37:58 +0100

    remove obsolete command from build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -eux
 
-cp -r assets/ ermintrude/src/main/web/
-
 pushd ermintrude
   npm install --prefix src/main/web/ermintrude --unsafe-perm
   mvn -Dmaven.test.skip clean package dependency:copy-dependencies
